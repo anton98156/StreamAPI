c88cdb92641ede09102a7211c8647d42fd75498d
Антон Кравцов <antonkravcov@MacBook-Air-Anton.local>
2023-11-25 10:59:51 +0300

    The task is done

---
+++ src/main/java/com/example/stream/Main.java
@@ -0,0 +1,18 @@
+package com.example.stream;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        
+        double averageOfEvenNumbers = numbers.stream()
+                .filter(num -> num % 2 == 0)
+                .mapToDouble(Integer::doubleValue)
+                .average()
+                .orElse(0);
+        
+        System.out.println("Среднее значение всех четных чисел в списке: " + averageOfEvenNumbers);
+    }
+}
